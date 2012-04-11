26070a8b69ef8a3c951af77e20b2ec1716d0ee84
chudley <aguycalledrichard@gmail.com>
2012-04-11 18:54:01 +0100

    Added minor functionality, such as deleting (with warning) and adding new colors from the page

--- colors.js
+++ colors.js
@@ -6,8 +6,26 @@ if(Meteor.is_client){
 	};
 	
 	Template.color_list.events = {
-		'click button': function(){
-			Colors.update(Session.get("session_color"), {$inc: {likes: 1}});
+		'click button.like': function(){
+			var color = Colors.findOne({_id: Session.get("session_color")})
+			Colors.update(color, {$inc: {likes: 1}});
+			console.log("Liked " + color.name + " (" + color._id + ")");
+		},
+		'click button.delete': function(){
+			var color = Colors.findOne({_id: Session.get("session_color")})
+			var answer = confirm("Delete the color " + color.name + "?")
+			if(answer){
+				Colors.remove(color)
+				console.log("Deleted " + color.name + " (" + color._id + ")");
+			}
+		},
+		'keyup #new': function(evt, text){
+			if(evt.which == 13){
+				var color = Colors.findOne({_id: Colors.insert({name: $('#new').val()})});
+
+				console.log("Added " + color.name + " (" + color._id + ")");
+			}
+
 		}
 	};
 
@@ -26,6 +44,7 @@ if(Meteor.is_client){
 	Template.color_info.events = {
 		'click': function(){
 			Session.set("session_color", this._id);
+			console.log("session_color changed to " + this._id);
 		}
 	};
 }
