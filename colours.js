ef8acfef09f657daaf49e3d955a5f1ea6e2b8d7a
chudley <aguycalledrichard@gmail.com>
2012-04-11 19:18:26 +0100

    Changed wording

--- colours.js
+++ colours.js
@@ -34,7 +34,7 @@ if(Meteor.is_client){
 	};
 
 	Template.colour_info.how_many = function(){
-		if(!this.likes) return "No people like";
+		if(!this.likes) return "Nobody likes";
 		if(this.likes == 1) return "One person likes";
 		if(this.likes < 5) return "A few people like";
 		if(this.likes < 20) return "Some people like";
